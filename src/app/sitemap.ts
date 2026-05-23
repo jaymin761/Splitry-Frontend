17e9d7e3e812f380619d3857c4e725b4195bfccd
jaymin761 <81026618+jaymin761@users.noreply.github.com>
2026-05-23 15:13:46 +0530

    changes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://splitry.com',
+      url: 'https://splitry.com/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1.0,
