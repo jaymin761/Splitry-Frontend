5f80808264dfd8d381873d8f5ba01935ef563bf1
jaymin761 <81026618+jaymin761@users.noreply.github.com>
2026-05-24 16:25:27 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -127,7 +127,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-XXXXXXXXXX');
+            gtag('config', 'G-DVG4Y2BNCZ');
           `}
         </Script>
       </body>
