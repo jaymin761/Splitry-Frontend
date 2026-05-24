46a65bfe4690b3f61c766d7c1936a26000fe0fdd
jaymin761 <81026618+jaymin761@users.noreply.github.com>
2026-05-24 16:30:50 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -120,9 +120,9 @@ export default function RootLayout({
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-DVG4Y2BNCZ"
-          strategy="lazyOnload"
+          strategy="afterInteractive"
         />
-        <Script id="google-analytics" strategy="lazyOnload">
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
