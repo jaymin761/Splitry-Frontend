6e1536a417a8b07f51b1145f2cd88c7b236ae44d
jaymin761 <81026618+jaymin761@users.noreply.github.com>
2026-06-04 21:15:09 +0530

    feat: add sitemap.ts configuration for SEO optimization

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,34 +1,42 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const currentDate = new Date();
+
   return [
     {
       url: 'https://splitry.com/',
+      lastModified: currentDate,
       priority: 1,
       changeFrequency: 'weekly',
     },
     {
       url: 'https://splitry.com/about',
+      lastModified: currentDate,
       priority: 0.8,
       changeFrequency: 'monthly',
     },
     {
       url: 'https://splitry.com/faq',
+      lastModified: currentDate,
       priority: 0.8,
       changeFrequency: 'monthly',
     },
     {
       url: 'https://splitry.com/contact',
+      lastModified: currentDate,
       priority: 0.5,
       changeFrequency: 'monthly',
     },
     {
       url: 'https://splitry.com/privacy-policy',
+      lastModified: currentDate,
       priority: 0.5,
       changeFrequency: 'yearly',
     },
     {
       url: 'https://splitry.com/terms',
+      lastModified: currentDate,
       priority: 0.5,
       changeFrequency: 'yearly',
     },
